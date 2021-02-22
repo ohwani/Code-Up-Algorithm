e89a4c4d66bd8edf8447d7b5b5b30d7aec629e34
ohwani <ohwani7@gmail.com>
2021-02-22 17:58:45 +0900

    신규 아이디 추천

--- Lv1/신규 아이디 추천.py
+++ Lv1/신규 아이디 추천.py
@@ -11,11 +11,25 @@ import re
 
 import re
 
-def solution(newid):
-    text = re.sub('[-_~!@#$%^&*()=+{}:?,<>/]', '', newid)
+def solution(new_id):
+
+    # 1. 특수문자 제거
+    text = re.sub(r'[~!@#$%^&*()=+\[{}\]:?,<>/]', '', new_id)
+    # 2. 대문자 -> 소문자로 변경
     text = text.lower()
-    if text[0] == '.':
-        print("아")
+    # 3. .. ... -> . 으로 변경
+    text = re.sub(r'\.+','.',text)
+    # 4. 맨앞 맨뒤 . 제거
+    text = re.sub(r'^\.|\.$','',text)
+    # 5. 맨뒤에 . 있을시 제거, 문자 길이는 15자 로 표현
+    text = re.sub(r'\.$', '', text[0:15])
+    # 6. 아이디가 정보가 아무것도 없을때 'a'로 저장
+    if text == '':
+        text = 'a'
+    # 7. 아이디의 길이가 3자 이하일경우 맨뒤글자 반복에서 3자 이상으로 만들어준다.
+    while len(text) < 3:
+        text += text[-1]
+
     return print(text)
 
-solution('...!@BaT#*..y.abcdefghijklm')
+solution('[z]-+.^.')
